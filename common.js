dffc337d182adc9bb707697758b77c1491b469fa
Andy Portmen <andy.portmen@gmail.com>
2017-09-27 18:12:50 +0330

    version 0.2.2

---
+++ common.js
@@ -0,0 +1,244 @@
+/* globals Parser */
+'use strict';
+
+var application = 'com.add0n.node';
+
+var notify = message => chrome.notifications.create(null, {
+  type: 'basic',
+  iconUrl: '/data/icons/48.png',
+  title: 'Save all Images',
+  message
+});
+
+function error(response) {
+  window.alert(`Something went wrong!
+
+-----
+Code: ${response.code}
+Output: ${response.stdout}
+Error: ${response.stderr}`);
+}
+
+function response(res) {
+  // windows batch returns 1
+  if (res && (res.code !== 0 && (res.code !== 1 || res.stderr !== ''))) {
+    error(res);
+  }
+  else if (!res) {
+    chrome.tabs.create({
+      url: '/data/helper/index.html'
+    });
+  }
+}
+
+function update() {
+  chrome.storage.local.get({
+    apps: {},
+    active: null
+  }, prefs => {
+    if (prefs.active) {
+      const app = prefs.apps[prefs.active];
+      chrome.browserAction.setIcon({
+        path: app.icon
+      });
+      chrome.browserAction.setTitle({
+        title: app.name
+      });
+    }
+    else {
+      chrome.browserAction.setIcon({
+        path: {
+          '16': 'data/icons/16.png',
+          '32': 'data/icons/32.png',
+          '64': 'data/icons/64.png'
+        }
+      });
+      chrome.browserAction.setTitle({
+        title: 'External Application Button'
+      });
+    }
+    chrome.contextMenus.removeAll(() => {
+      Object.keys(prefs.apps).filter(k => {
+        const context = prefs.apps[k].context;
+        if (!context) {
+          return false;
+        }
+        if (typeof context === 'string') {
+          return context;
+        }
+        else {
+          return context.length;
+        }
+      }).forEach(id => {
+        const anyWhere = ['*://*/*', 'file://*/*', 'ftp://*/*'];
+        let pattern = anyWhere;
+        if (prefs.apps[id].pattern) {
+          const tmp = prefs.apps[id].pattern;
+          pattern = tmp.split(/\s*,\s*/);
+        }
+        let contexts = prefs.apps[id].context;
+        if (typeof contexts === 'string') {
+          contexts = [contexts];
+        }
+        const pageContexts = contexts.filter(s => ['page', 'tab', 'selection'].indexOf(s) !== -1);
+        const linkContexts = contexts.filter(s => ['page', 'tab', 'selection'].indexOf(s) === -1);
+
+        function add(obj) {
+          chrome.contextMenus.create(obj, () => {
+            const lastError = chrome.runtime.lastError;
+            if (lastError) {
+              notify(lastError.message, contexts);
+            }
+          });
+        }
+
+        if (pageContexts.length) {
+          add({
+            id: id + '-page',
+            title: prefs.apps[id].name,
+            contexts: pageContexts,
+            documentUrlPatterns: pattern
+          });
+        }
+        if (linkContexts.length) {
+          add({
+            id: id + '-link',
+            title: prefs.apps[id].name,
+            contexts: linkContexts,
+            documentUrlPatterns: anyWhere,
+            targetUrlPatterns: pattern
+          });
+        }
+      });
+      Object.keys(prefs.apps).filter(k => prefs.apps[k].toolbar).forEach(id => {
+        chrome.contextMenus.create({
+          id: 'change-to-' + id,
+          title: prefs.apps[id].name,
+          contexts: ['browser_action'],
+        });
+      });
+    });
+  });
+}
+
+function argv(app, url, selectionText) {
+  const termref = {
+    lineBuffer: app.args.replace(/\[HREF\]/g, url)
+      .replace(/\[HOSTNAME\]/g, url.hostname)
+      .replace(/\[PATHNAME\]/g, url.pathname)
+      .replace(/\[HASH\]/g, url.hash)
+      .replace(/\[PROTOCOL\]/g, url.protocol)
+      .replace(/\[SELECTIONTEXT\]/g, selectionText)
+      .replace(/\\/g, '\\\\')
+  };
+  const parser = new Parser();
+  parser.parseLine(termref);
+
+  if (app.quotes) {
+    termref.argv = termref.argv.map((a, i) => {
+      if (termref.argQL[i]) {
+        return termref.argQL[i] + a + termref.argQL[i];
+      }
+      return a;
+    });
+  }
+  return termref.argv;
+}
+
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+  if (request.method === 'parse') {
+    response(argv(request.app, new URL('http://example.com/index.html'), 'Sample selected text'));
+  }
+});
+
+function execute(app, url, selectionText) {
+  chrome.runtime.sendNativeMessage(application, {
+    cmd: 'exec',
+    command: app.path,
+    arguments: argv(app, url, selectionText),
+    properties: app.quotes ? {windowsVerbatimArguments: true} : {}
+  }, app.errors ? () => {} : response);
+}
+
+chrome.contextMenus.onClicked.addListener(info => {
+  let id = info.menuItemId;
+  if (id.startsWith('change-to-')) {
+    chrome.storage.local.set({
+      active: id.replace('change-to-', '')
+    });
+  }
+  else {
+    id = id.replace('-page', '').replace('-link', '');
+    chrome.storage.local.get({
+      apps: {}
+    }, prefs => {
+      const app = prefs.apps[id];
+      const selectionText = info.selectionText;
+      let url = info.pageUrl;
+      if (app.context === 'page') {
+        url = info.pageUrl;
+      }
+      else if (app.context === 'frame') {
+        url = info.frameUrl;
+      }
+      else if (app.context === 'selection') {
+        url = info.frameUrl || info.pageUrl;
+      }
+      else if (app.context === 'link') {
+        url = info.linkUrl || info.frameUrl || info.pageUrl;
+      }
+      else if (app.context === 'image' || app.context === 'video' || app.context === 'audio') {
+        url = info.srcUrl || info.linkUrl || info.frameUrl || info.pageUrl;
+      }
+      execute(app, url, selectionText);
+    });
+  }
+});
+
+(function(callback) {
+  chrome.runtime.onInstalled.addListener(callback);
+  chrome.runtime.onStartup.addListener(callback);
+})(update);
+
+chrome.storage.onChanged.addListener(prefs => {
+  if (prefs.active || prefs.apps) {
+    update();
+  }
+});
+
+chrome.browserAction.onClicked.addListener(tab => {
+  chrome.storage.local.get({
+    active: null,
+    apps: {}
+  }, prefs => {
+    if (prefs.active) {
+      let url = {};
+      try {
+        url = new URL(tab.url);
+      }
+      catch (e) {}
+      execute(prefs.apps[prefs.active], url);
+    }
+    else {
+      chrome.runtime.openOptionsPage();
+    }
+  });
+});
+
+// FAQs & Feedback
+chrome.storage.local.get('version', prefs => {
+  const version = chrome.runtime.getManifest().version;
+  const isFirefox = navigator.userAgent.indexOf('Firefox') !== -1;
+  if (isFirefox ? !prefs.version : prefs.version !== version) {
+    chrome.storage.local.set({version}, () => {
+      chrome.tabs.create({
+        url: 'http://add0n.com/external-application-button.html?version=' + version +
+          '&type=' + (prefs.version ? ('upgrade&p=' + prefs.version) : 'install')
+      });
+    });
+  }
+});
+{
+  const {name, version} = chrome.runtime.getManifest();
+  chrome.runtime.setUninstallURL('http://add0n.com/feedback.html?name=' + name + '&version=' + version);
+}
---
+++ data/options/index.js
@@ -0,0 +1,214 @@
+'use strict';
+
+var app = document.getElementById('app');
+var list = document.getElementById('list');
+var message = document.getElementById('message');
+var remove = document.getElementById('remove');
+var add = document.getElementById('add');
+var preview = document.getElementById('preview');
+var form = {
+  path: app.querySelector('[data-id=path]'),
+  name: app.querySelector('[data-id=name]'),
+  args: app.querySelector('[data-id=arguments]'),
+  toolbar: app.querySelector('[data-id=toolbar]'),
+  menuitem: app.querySelector('[data-id=menuitem]'),
+  context: app.querySelector('[data-id=context]'),
+  pattern: app.querySelector('[data-id=pattern]'),
+  icon: app.querySelector('[data-id=icon]'),
+  errors: app.querySelector('[data-id=errors]'),
+  quotes: app.querySelector('[data-id=quotes]')
+};
+
+var id;
+
+function show(msg) {
+  window.clearTimeout(id);
+  message.textContent = msg;
+  id = window.setTimeout(() => message.textContent = '', 2000);
+}
+
+function update(value = 'blank') {
+  list.textContent = '';
+  chrome.storage.local.get({
+    apps: {}
+  }, prefs => {
+    prefs.apps = Object.assign({
+      blank: {
+        name: '- new -'
+      }
+    }, prefs.apps);
+    Object.keys(prefs.apps).forEach(id => {
+      const option = document.createElement('option');
+      option.value = id;
+      option.textContent = prefs.apps[id].name;
+      list.appendChild(option);
+    });
+    list.value = value;
+    list.dispatchEvent(new Event('change'));
+  });
+}
+update();
+
+function save({id, icon, errors, quotes, name, path, args, toolbar, context, pattern}) {
+  pattern = (pattern || '').split(/\s*,\s*/).filter((s, i, l) => l.indexOf(s) === i).join(', ');
+  chrome.storage.local.get({
+    apps: {}
+  }, prefs => {
+    prefs.apps[id] = {
+      icon,
+      errors,
+      quotes,
+      name,
+      path,
+      args,
+      toolbar,
+      context,
+      pattern
+    };
+    chrome.storage.local.set(prefs, () => {
+      update(id);
+      show('Done!');
+    });
+  });
+}
+
+function collect(callback) {
+  const id = app.dataset.id || Math.random();
+
+  const name = form.name.value;
+  if (!name) {
+    return show('"Display Name" is mandatory');
+  }
+  const path = form.path.value;
+  if (!path) {
+    return show('"Executable Name" is mandatory');
+  }
+  const args = form.args.value;
+  const toolbar = form.toolbar.checked;
+  const menuitem = form.context.querySelector(':checked');
+  if (!toolbar && !menuitem) {
+    return show('Select a placement for this application');
+  }
+  const context = [...form.context.querySelectorAll(':checked')].map(e => e.value);
+  const pattern = form.pattern.value;
+  const errors = form.errors.checked;
+  const quotes = form.quotes.checked;
+  const icon = form.icon.files[0];
+  if (!icon && !app.dataset.file) {
+    return show('"Icon" is mandatory');
+  }
+
+  const s = {id, name, errors, quotes, path, args, toolbar, context, pattern};
+  if (icon) {
+    if (icon.size > 5 * 1024) {
+      return show('"Icon" is too big! use 16x16 PNG.');
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      s.icon = reader.result;
+      callback(s);
+    };
+    reader.readAsDataURL(icon);
+  }
+  else {
+    s.icon = app.dataset.file;
+    callback(s);
+  }
+}
+
+document.addEventListener('click', e => {
+  const target = e.target;
+  const cmd = target.dataset.cmd;
+  if (cmd === 'add') {
+    collect(save);
+  }
+  else if (cmd === 'remove') {
+    chrome.storage.local.get({
+      apps: {},
+      active: null
+    }, prefs => {
+      delete prefs.apps[list.value];
+      if (prefs.active === list.value) {
+        prefs.active = null;
+      }
+      chrome.storage.local.set(prefs, update);
+    });
+  }
+  else if (cmd === 'insert') {
+    const text = target.dataset.value;
+    const startPos = form.args.selectionStart;
+    const endPos = form.args.selectionEnd;
+    form.args.value = form.args.value.substring(0, startPos) +
+      text +
+      form.args.value.substring(endPos, form.args.value.length);
+    form.args.selectionStart = startPos + text.length;
+    form.args.selectionEnd = startPos + text.length;
+    form.args.focus();
+  }
+  else if (cmd === 'example') {
+    form.name.value = target.dataset.name;
+    form.path.value = target.dataset.path;
+    form.args.value = target.dataset.args;
+  }
+  else if (cmd === 'test') {
+    collect(app => {
+      console.log(app);
+      chrome.runtime.sendMessage({
+        method: 'parse',
+        app
+      }, resp => {
+        const doc = preview.contentDocument;
+        doc.body.textContent = '';
+        const ul = document.createElement('ul');
+        resp.forEach(s => ul.appendChild(Object.assign(doc.createElement('li'), {
+          textContent: s
+        })));
+        doc.body.appendChild(ul);
+        preview.style.display = 'block';
+      });
+    });
+  }
+});
+
+document.body.addEventListener('click', () => {
+  preview.style.display = 'none';
+});
+
+list.addEventListener('change', () => {
+  const disabled = list.selectedIndex === -1 || list.selectedIndex === 0;
+  remove.disabled = disabled;
+  add.value = disabled ? 'Add Application' : 'Update Application';
+  if (!disabled) {
+    chrome.storage.local.get({
+      apps: {}
+    }, prefs => {
+      form.name.value = prefs.apps[list.value].name;
+      form.errors.checked = prefs.apps[list.value].errors;
+      form.quotes.checked = prefs.apps[list.value].quotes;
+      form.path.value = prefs.apps[list.value].path;
+      form.args.value = prefs.apps[list.value].args;
+      form.toolbar.checked = prefs.apps[list.value].toolbar;
+      [...form.context.querySelectorAll(':checked')].forEach(e => e.checked = false);
+      let contexts = prefs.apps[list.value].context;
+      if (typeof contexts === 'string') {
+        contexts = [contexts];
+      }
+      contexts.forEach(value => {
+        form.context.querySelector(`[value="${value}"]`).checked = true;
+      });
+      form.pattern.value = prefs.apps[list.value].pattern || '';
+      app.dataset.file = prefs.apps[list.value].icon;
+      form.icon.value = '';
+      app.dataset.id = list.value;
+      if (prefs.apps[list.value].toolbar) {
+        chrome.storage.local.set({
+          active: list.value
+        });
+      }
+    });
+  }
+  else {
+    delete app.dataset.file;
+    delete app.dataset.id;
+  }
+});
