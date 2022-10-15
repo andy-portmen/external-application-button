e74101abb1d38012f5bf52471ae466565dbe8733
Andy Portmen <andy.portmen@gmail.com>
2022-10-15 11:06:20 -0400

    fixes #74

--- v3/data/redirect/index.js
+++ v3/data/redirect/index.js
@@ -3,6 +3,7 @@ const args = new URLSearchParams(location.search);
 chrome.runtime.sendMessage({
   method: 'execute',
   id: args.get('key'),
-  href: args.get('href'),
+  // https://github.com/andy-portmen/external-application-button/issues/74
+  href: location.search.replace(/^[^&]*&href=/, ''),
   selectionText: ''
 }, () => history.back());
